dadabc61e6599afee72aaa644d757f9f93e5cd76
Mykyta Stambul <stambul.n@gmail.com>
2025-01-19 15:50:10 -0800

    setup deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/news-explorer-frontend/",
+  base: "/",
   server: {
     port: 3000,
   },
